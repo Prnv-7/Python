3dd302c630f121a2ad52e3f41a10ef6232aae1e2
Pranav Suresh <118801338+Prnv-7@users.noreply.github.com>
2023-03-29 22:07:20 +0530

    Update String.py

--- String.py
+++ String.py
@@ -1,1 +1,6 @@
 # This file contains all the built-in functions available to manipulate string.
+ string = 'hello'
+
+  # 1) Capitalize() :
+  string.capitalize()
+  # Capitalizes the first letter of the string.
