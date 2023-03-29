d1321bf0f380efc69c514986504c4ad8220586ea
Pranav Suresh <118801338+Prnv-7@users.noreply.github.com>
2023-03-29 22:17:05 +0530

    Update String.py

--- String.py
+++ String.py
@@ -1,6 +1,30 @@
 # This file contains all the built-in functions available to manipulate string.
  string = 'hello'
 
-  # 1) Capitalize() :
-  string.capitalize()
-  # Capitalizes the first letter of the string.
+ # 1) Capitalize() :
+ string.capitalize()
+ # Capitalizes the first letter of the string.
+
+ # 2) upper():
+ string.upper()
+ # Converts every letter to uppercase.
+ 
+ # 3) lower():
+ string.lower()
+ # Converts every letter to lowercase.
+ 
+  # 4) isupper():
+ string.isupper()
+ # Returns True if the string has at least one uppercase character.
+ 
+ # 5) islower():
+ string.islower()
+ # Returns True if the string has at least one lowercase character.
+ 
+ # 6) len():
+ len(string)
+ # Returns the total length of the string.
+ 
+  # 7) split():
+ string.split()
+ # Splits the string according to the delimiter provided.
